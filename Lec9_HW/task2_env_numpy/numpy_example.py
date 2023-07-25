afdc87fa8ca25bd505ac674cd9be7f794d2cbb31
Victoria Nesterenko <DanilDanill@gmail.com>
2023-07-25 21:17:52 +0300

    Add task2

---
+++ Lec9_HW/task2_env_numpy/numpy_example.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print("Original array:", arr)
+print("Sum of elements:", np.sum(arr))
+print("Mean of elements:", np.mean(arr))
+print("Standard deviation of elements:", np.std(arr))
