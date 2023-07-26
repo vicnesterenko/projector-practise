bd6dbae1fe7f45968efdcdadd28d4541311f10f2
Victoria Nesterenko <DanilDanill@gmail.com>
2023-07-26 23:20:32 +0300

    Add HW10

--- Lec10_HW/Task3_4.py
+++ Lec10_HW/Task3_4.py
@@ -4,7 +4,7 @@ import csv
 PLAYERS = ["Josh", "Luke", "Kate", "Mark", "Mary"]
 
 
-def generate_rate():
+def generate_rate() -> list:
     rate = []
     for i in range(0, 100):
         for name in PLAYERS:
@@ -13,7 +13,7 @@ def generate_rate():
     return rate
 
 
-def save_in_csv(rate: list):
+def save_in_csv(rate: list) -> csv:
     rate_csv = "Projector-practise\Lec10_HW\scores.csv"
     with open(rate_csv, "w") as csvfile:
         csvfile = csv.writer(csvfile)
@@ -23,24 +23,26 @@ def save_in_csv(rate: list):
     return rate_csv
 
 
-def generate_high_scores(rate_csv):
-    high_scores = {}  # Dictionary to store the high score for each name
+def generate_high_scores(rate_csv: csv) -> dict:
+    high_scores = {}
     with open(rate_csv, mode="r") as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             name = row["Name"]
-            score = int(row["Score"])  # Convert score to an integer
+            score = int(row["Score"])
             if name not in high_scores or score > high_scores[name]:
                 high_scores[name] = score
         return high_scores
 
 
-def save_highscore_in_csv(high_dict: dict):
+def save_highscore_in_csv(high_dict: dict) -> csv:
     high_scores = "Projector-practise/Lec10_HW/high_scores.csv"
     with open(high_scores, "w", newline="") as csvfile:
         csv_writer = csv.writer(csvfile)
         csv_writer.writerow(["Player name", "Highest score"])
-        for name, highest_score in sorted(high_dict.items()):
+        for name, highest_score in sorted(
+            high_dict.items(), key=lambda item: item[1], reverse=True
+        ):
             csv_writer.writerow([name, highest_score])
     return high_scores
 
