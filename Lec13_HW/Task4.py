4751ec08e1d8892f6676ce29368046aac6d334e0
Victoria Nesterenko <DanilDanill@gmail.com>
2023-08-10 22:48:57 +0300

    In progress

---
+++ Lec13_HW/Task4.py
@@ -0,0 +1,14 @@
+class Robot:
+    def __init__(self, orientation: str, position_x, position_y):
+        self.orientation = orientation
+        self.position_x = position_x
+        self.position_y = position_y
+
+    def move(self, steps: str):
+        pass
+
+    def turn(self, direction: str):
+        pass
+
+    def display_position(self):
+        pass
