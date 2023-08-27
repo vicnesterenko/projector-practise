52601155be0561044acf2bcdf04feafc82d1fc99
Victoria Nesterenko <DanilDanill@gmail.com>
2023-08-27 13:25:43 +0300

    Add

--- Lec14_HW/Task3.py
+++ Lec14_HW/Task3.py
@@ -32,16 +32,17 @@ class Account:
 
 class SavingsAccount(Account):
     def __init__(self, balance, account_number, interest):
-        super.__init__(balance, account_number)
+        super().__init__(balance, account_number)
         self.interest = interest
 
     def add_interest_to_account(self):
         self._balance += self._balance * (self.interest / 100)
+        return self._balance
 
 
 class CurrentAccount(Account):
     def __init__(self, balance, account_number, limit):
-        super.__init__(balance, account_number)
+        super().__init__(balance, account_number)
         self.limit = limit
 
 
@@ -49,11 +50,33 @@ class Bank:
     def __init__(self, accounts: list[Account]):
         self.accounts = accounts
 
-    def open_account(self):
-        pass
+    def open_account(self, account):
+        self.accounts.append(account)
 
-    def close_account(self):
-        pass
+    def close_account(self, account):
+        self.accounts.remove(account)
 
     def pay_dividends(self):
-        pass
+        for account in self.accounts:
+            if isinstance(account, SavingsAccount):
+                account.add_interest_to_account()
+            elif isinstance(account, CurrentAccount):
+                if account.get_balance() < 0:
+                    print(
+                        f"Letter sent for overdraft on account {account.get_account_number()}"
+                    )
+
+
+if __name__ == "__main__":
+    account1 = SavingsAccount(1000, "1", 0)
+    account2 = CurrentAccount(500, "2", -10000)
+    account3 = Account.create_account("3")
+
+    bank = Bank([account1, account2, account3])
+
+    bank.pay_dividends()
+
+    print(account1)
+    print(account2)
+    print(account3)
+    bank.close_account(account3)
